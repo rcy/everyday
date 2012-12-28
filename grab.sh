063a649fcc54d96c4380de908f788fefd091de93
Ryan Yeske <rcyeske@gmail.com>
2012-12-28 02:22:38 -0800

    output result

--- grab.sh
+++ grab.sh
@@ -5,4 +5,7 @@ dir=${base}/`date +%Y`/`date +%m`/`date +%d`
 filename=${dir}/`date +%Y%m%d-%H%M%S`.jpg
 
 mkdir -p $dir
+
 /usr/bin/fswebcam --quiet --resolution 640x480 $filename
+
+echo grab.sh: output image to $filename
