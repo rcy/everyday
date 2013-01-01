9b5131a75356d6b4dec682debe02adb18bfdf99d
Ryan Yeske <rcyeske@gmail.com>
2013-01-01 15:42:06 -0800

    change styles, highlight playing video on calendar

--- bin/mkvideosjs.sh
+++ bin/mkvideosjs.sh
@@ -4,7 +4,11 @@ echo "var _videoDB = {"
 
 for file in $@; do 
     date=$(echo $file|cut -d/ -f2,3,4)
-    echo "'$date': {file:'../$file'},"
+    duration=$(ffmpeg -i $file 2>&1 | awk '/Duration/ {split($2, A, "."); print A[1]}')
+    echo -n "  '$date': {"
+    echo -n " file:'../$file'"
+    echo -n ", duration:'$duration'"
+    echo " },"
 done
 
 echo "};"
--- public/index.js
+++ public/index.js
@@ -1,24 +1,28 @@
+// the event that corresponds with the currently playing video
+var playingEvent = null;
+
 $("#calendar").fullCalendar({
   header: {
     left: 'title',
     center: '',
     right: 'today prev,next'
   },
-  dayClick: dayClick,
-  events: getEvents
+  eventClick: eventClick,
+  events: getEvents,
 })
 
 function getEvents(start, end, done) {
-  console.log('getEvents', start, end);
   var events = [];
+
   // for days between start end, collect files
   var today = start;
   while (today < end) {
     var rec = getVideoRecord(today);
     if (rec)
-      events.push({date: today, title: 'vid'});
+      events.push({date: today, title: rec.duration, file: rec.file});
     today = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
   }
+
   done(events);
 }
 
@@ -27,18 +31,32 @@ function getVideoRecord(date) {
   return _videoDB[key];
 }
 
-function dayClick(date, allday, e, view) {
-  var file = getVideoRecord(date).file;
+function eventClick(event, jsEvent, view){
+  if (playingEvent) {
+    playingEvent.backgroundColor = '';
+    $("#calendar").fullCalendar('updateEvent', playingEvent);
+  }
+
+  playingEvent = event;
+  playingEvent.backgroundColor = 'red';
+  $("#calendar").fullCalendar('updateEvent', playingEvent);
+
+  playVideo(event.start);
+}
+
+function playVideo(date) {
+  var rec = getVideoRecord(date);
 
   _V_('video-player').ready(function(){ 
     var player = this;
     
     // change track to day clicked
-    player.src({type: 'video/mp4', src: file});
+    player.src({type: 'video/mp4', src: rec.file});
 
     // start video
     player.play() 
   });
 
-  $('#video-title').text(file);
+  // update title with day of playing video
+  $("#title .playing").text($.fullCalendar.formatDate(date, "dddd MMMM d, yyyy"));
 }
