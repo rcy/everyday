ccbb1845a22265ee82ee95389761c489aa00de2d
Ryan Yeske <rcyeske@gmail.com>
2013-01-01 16:33:46 -0800

    style gitignore videos.js

--- public/videos.js
+++
@@ -1,8 +0,0 @@
-var _videoDB = {
-  '2012/12/27': { file:'../videos/2012/12/27/movie.mp4', duration:'00:00:10' },
-  '2012/12/28': { file:'../videos/2012/12/28/movie.mp4', duration:'00:00:34' },
-  '2012/12/29': { file:'../videos/2012/12/29/movie.mp4', duration:'00:00:27' },
-  '2012/12/30': { file:'../videos/2012/12/30/movie.mp4', duration:'00:00:31' },
-  '2012/12/31': { file:'../videos/2012/12/31/movie.mp4', duration:'00:00:55' },
-  '2013/01/01': { file:'../videos/2013/01/01/movie.mp4', duration:'00:00:28' },
-};
